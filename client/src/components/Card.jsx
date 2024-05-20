75a51f55f38454810b005dafae9412aebac5daac
ryukennz <fernandordyansyah@gmail.com>
2024-05-20 21:48:38 +0700

    client: fetching and search (ongoing) refactor

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -2,61 +2,40 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "../utils/axios";
 
-export default function Card() {
-  const [movies, setMovies] = useState([]);
-  const [search, setSearch] = useState("");
-  const getData = async () => {
-    try {
-      const { data } = await axios({
-        method: "GET",
-        url: "/api/movies",
-      });
-      setMovies(data.movies);
-      // console.log(response, "<< CEK");
-    } catch (error) {
-      console.log(error);
-    }
-  };
+export default function Card({ fetchData, movies }) {
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.name.toLowerCase().includes(search.toLowerCase())
+  );
 
   useEffect(() => {
-    getData();
+    fetchData();
   }, []);
 
   return (
     <>
       <div className="p-10 bg-[#0f0f0f]">
-        <div className="">
-          <input
-            type="text"
-            placeholder="Search here..."
-            className="w-full p-3 mb-6 text-lg text-neutral-800 placeholder-neutral-500 bg-black rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <div className="sm:grid grid-cols-2 gap-10 md:grid-cols-2 lg:grid-cols-3">
-            {movies?.map((movie, index) => {
-              return (
-                <>
-                  <div
-                    key={index}
-                    className="rounded-3xl group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30"
-                  >
-                    <div className="h-96 w-72 ">
-                      <img
-                        className="h-full w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
-                        src={movie.img}
-                        alt=""
-                      />
-                    </div>
-                    <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70" />
-                    <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center text-center transition-all duration-500 group-hover:translate-y-0">
-                      <h1 className="font-[open-sans] text-2xl font-bold text-white">
-                        {movie.name}
-                      </h1>
-                    </div>
-                  </div>
-                </>
-              );
-            })}
-          </div>
+        <div className="grid md:grid-cols-2 gap-10 lg:grid-cols-3">
+          {filteredMovies?.map((movie, index) => (
+            <div
+              key={index}
+              className="rounded-3xl group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-black/30"
+            >
+              <div className="h-96 w-72">
+                <img
+                  className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-125"
+                  src={movie.img}
+                  alt={movie.name}
+                />
+              </div>
+              <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70" />
+              <div className="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center text-center transition-all duration-500 group-hover:-translate-y-6">
+                <h1 className="font-[open-sans] text-2xl font-bold text-white">
+                  {movie.name}
+                </h1>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,35 +1,58 @@
 import Card from "../components/Card";
+import { useState } from "react";
+import axios from '../utils/axios'
 export default function HomePage() {
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  const getData = async () => {
+    try {
+      const { data } = await axios({
+        method: "GET",
+        url: "/api/movies",
+      });
+      setMovies(data.movies);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+  };
+
+  
+
   return (
     <>
-      <div>
-  <div className="bg-black flex items-center p-4">
-    <img
-      className="w-24 mx-auto"
-      src="https://vip.idlixofficial.net/wp-content/uploads/2020/07/logov4.png"
-      alt="Logo"
-    />
-    <div className="relative w-full max-w-md ml-4">
-      <input
-        className="w-full p-3 text-md text-white placeholder-neutral-500 bg-[#0f0f0f] rounded-lg focus:outline-none"
-        type="text"
-        placeholder="Search..."
-      />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="absolute right-4 top-1/2 transform -translate-y-1/2 h-6 w-6 text-neutral-500"
-        viewBox="0 0 30 30"
-        fill="currentColor"
-      >
-        <path d="M 13 3 C 7.4886661 3 3 7.4886661 3 13 C 3 18.511334 7.4886661 23 13 23 C 15.396652 23 17.59741 22.148942 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148942 17.59741 23 15.396652 23 13 C 23 7.4886661 18.511334 3 13 3 z M 13 5 C 17.430666 5 21 8.5693339 21 13 C 21 17.430666 17.430666 21 13 21 C 8.5693339 21 5 17.430666 5 13 C 5 8.5693339 8.5693339 5 13 5 z"></path>
-      </svg>
-    </div>
-  </div>
-  <div className="min-h-screen flex justify-center items-center bg-black">
-    <Card />
-  </div>
-</div>
-
+      <div className="bg-black flex items-center p-8 justify-evenly">
+        <img
+          className="w-24 lg:ml-24"
+          src="https://vip.idlixofficial.net/wp-content/uploads/2020/07/logov4.png"
+          alt="Logo"
+        />
+        <div className="relative w-full max-w-sm">
+          <input
+            onChange={handleChange}
+            value={search}
+            className="w-full p-3 pl-4 text-md text-white placeholder-neutral-500 bg-[#0f0f0f] rounded-lg focus:outline-none"
+            type="text"
+            placeholder="Search..."
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute right-3 top-1/2 transform -translate-y-1/2 h-6 w-6 text-neutral-500"
+            viewBox="0 0 30 30"
+            fill="currentColor"
+          >
+            <path d="M 13 3 C 7.4886661 3 3 7.4886661 3 13 C 3 18.511334 7.4886661 23 13 23 C 15.396652 23 17.59741 22.148942 19.322266 20.736328 L 25.292969 26.707031 A 1.0001 1.0001 0 1 0 26.707031 25.292969 L 20.736328 19.322266 C 22.148942 17.59741 23 15.396652 23 13 C 23 7.4886661 18.511334 3 13 3 z M 13 5 C 17.430666 5 21 8.5693339 21 13 C 21 17.430666 17.430666 21 13 21 C 8.5693339 21 5 17.430666 5 13 C 5 8.5693339 8.5693339 5 13 5 z"></path>
+          </svg>
+        </div>
+      </div>
+      <div className="min-h-screen flex justify-center items-center bg-black">
+        <Card search={search} fetchData={getData} movies={movies} />
+      </div>
     </>
   );
 }
